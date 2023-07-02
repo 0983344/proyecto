da7ce4a186aecb4e394814b4dbda9754978e5921
robin <robin@ROBINSON>
2023-07-02 18:28:56 -0500

    Agregar archivos de proyecto.

---
+++ proyecto/proyecto/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace proyecto
+{
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.1.247/pawpal/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<pawpal> post;
+        public MainPage()
+        {
+            InitializeComponent();
+            ObtenerDatos();
+        }
+
+        public async void ObtenerDatos()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<pawpal> listPost = JsonConvert.DeserializeObject<List<pawpal>>(contenido);
+            post = new ObservableCollection<pawpal>(listPost);
+            ListaPawpal.ItemsSource = post;
+        }
+        private void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ proyecto/proyecto/pawpal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proyecto
+{
+    public class pawpal
+    {
+        public int codigo { set; get; }
+        public string nombrevacuna { set; get; }
+        public int edadmascota  { set; get; }
+        public DateTime fechavacuna { set; get; }
+        public string refuerzo { set; get; }
+        public string administradorvacuna { set; get; }
+        public string observaciones { set; get; }
+    }
+}
